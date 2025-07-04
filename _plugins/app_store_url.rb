5838de16851c85ec5db6e56217b36ea2bd4f61cc
Matias <mh@lilucat.com>
2025-07-04 15:56:37 +0200

    fix: remove custom app_store_url plugin and use clean App Store URLs in all includes/pages (GitHub Pages compatible)

--- _plugins/app_store_url.rb
+++
@@ -1,9 +0,0 @@
-module Jekyll
-  module AppStoreUrlFilter
-    def app_store_url(app_id)
-      "https://apps.apple.com/us/app/id#{app_id}?uo=4"
-    end
-  end
-end
-
-Liquid::Template.register_filter(Jekyll::AppStoreUrlFilter) 
